83838113189bc8059ff2dd07eadbeb8f367a7d3b
Rodrigo Galhardo <6187392+rodrigogalhardo@users.noreply.github.com>
2022-07-05 15:40:58 +0000

    Nova versão de HA Proxy para 2022

--- haproxy/haproxy_setup.sh
+++ haproxy/haproxy_setup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+
+# For latest versions of haproxy, see: https://hub.docker.com/r/haproxytech/haproxy-ubuntu
 docker run --name haproxy -d -p 8002:80 -v /tmp/haproxy.cfg:/etc/haproxy/haproxy.cfg haproxytech/haproxy-ubuntu:2.3.5
